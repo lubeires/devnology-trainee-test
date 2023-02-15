6cad9b9a80c283d71195fb3b2b0a96f7e08a10bc
Luana Palhares <luanaribeiropalhares@gmail.com>
2023-02-15 14:37:06 -0300

    fix error message

--- server/controllers/devgo.js
+++ server/controllers/devgo.js
@@ -4,7 +4,7 @@ const getDevGoArticles = async (req, res) => {
     const devGoArticles = await scrape();
     res.status(200).json(devGoArticles);
   } catch (err) {
-    return res.status(404).json({ error: err });
+    return res.status(404).json({ error: err.message });
   }
 };
 
